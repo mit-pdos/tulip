512384b1ffff3e3f38f43c7d458b4e2b12c7ac57
Yun-Sheng Chang <yschang@mit.edu>
2024-10-08 18:33:06 -0400

    Add an interactive paxos example running on localhost

---
+++ main/paxos-local/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+	"github.com/mit-pdos/gokv/grove_ffi"
+	"github.com/mit-pdos/tulip/paxos"
+)
+
+func main() {
+	addrm := make(map[uint64]grove_ffi.Address)
+
+	addrm[0] = grove_ffi.MakeAddress("127.0.0.1:49978")
+	addrm[1] = grove_ffi.MakeAddress("127.0.0.1:49979")
+	addrm[2] = grove_ffi.MakeAddress("127.0.0.1:49980")
+
+	pxs := make(map[uint64]*paxos.Paxos)
+	for nid := range(addrm) {
+		pxs[nid] = paxos.Start(nid, addrm)
+		fmt.Printf("[main] Paxos node %d started.\n", nid)
+	}
+
+	var leader uint64 = 0
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("[main] Enter command: submit <value> | lookup <idx>\n> ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		input := scanner.Text()
+
+		px := pxs[leader]
+
+		var value string
+		_, err := fmt.Sscanf(input, "submit %s", &value)
+		if err == nil {
+			lsn, term := px.Submit(value)
+			if term == 0 {
+				fmt.Printf("[main] Fail to submit to %d. Change leader.\n", leader)
+				leader = (leader + 1) % 3
+				continue
+			}
+
+			fmt.Printf("[main] Submit value \"%s\" to %d in term %d @ %d.\n",
+				value, leader, term, lsn)
+			continue
+		}
+
+		var idx uint64
+		_, err = fmt.Sscanf(input, "lookup %d", &idx)
+		if err == nil {
+			for nid, px := range(pxs) {
+				v, ok := px.Lookup(idx)
+				if ok {
+					fmt.Printf("[main] Value @ %d = %s (on node %d).\n", idx, v, nid)
+				} else {
+					fmt.Printf("[main] Value @ %d not chosen (on node %d).\n", idx, nid)
+				}
+			}
+			continue
+		}
+
+		fmt.Printf("[main] Command not recognized.\n")
+	}
+}
