f15347c7ae0084ca3723a4bcdc649ed78c6fc13a
Yun-Sheng Chang <yschang@mit.edu>
2024-12-04 22:33:34 -0500

    Tune the timing parameters

--- params/params.go
+++ params/params.go
@@ -2,20 +2,20 @@ package params
 
 const N_SHARDS uint64 = 2
 
-// Timeout for re-establishing connection: 10ms.
-const NS_RECONNECT uint64 = 10_000_000
+// Timeout for re-establishing connection: 100ms.
+const NS_RECONNECT uint64 = 100_000_000
 
 // Timeout for re-resending a PREPARE-family message: 500ms.
 const NS_RESEND_PREPARE uint64 = 500_000_000
 
-// Timeout for re-resending a READ message: 20ms.
-const NS_RESEND_READ uint64 = 20_000_000
+// Timeout for re-resending a READ message: 200ms.
+const NS_RESEND_READ uint64 = 200_000_000
 
-// Timeout for re-resending a COMMIT message: 40ms.
-const NS_RESEND_COMMIT uint64 = 40_000_000
+// Timeout for re-resending a COMMIT message: 200ms.
+const NS_RESEND_COMMIT uint64 = 200_000_000
 
-// Timeout for re-resending an ABORT message: 40ms.
-const NS_RESEND_ABORT uint64 = 40_000_000
+// Timeout for re-resending an ABORT message: 200ms.
+const NS_RESEND_ABORT uint64 = 200_000_000
 
 // Timeout for spawning off a backup coordinator: 5-6s (randomized).
 const NS_SPAWN_BACKUP_BASE  uint64 = 5_000_000_000
@@ -24,7 +24,7 @@ const NS_SPAWN_BACKUP_DELTA uint64 = 1_000_000_000
 // Time interval for sending REFRESH message: 4s.
 const NS_SEND_REFRESH uint64 = 4_000_000_000
 
-// Time interval for sending the batched Paxos commands: 5 ms.
+// Time interval for sending the batched Paxos commands: 300 ms.
 const NS_BATCH_INTERVAL uint64 = 300_000_000
 
 // Time interval for sending the batched Paxos commands: 1 s.
