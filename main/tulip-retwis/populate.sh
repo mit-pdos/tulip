7dd54a9607328aafb2a6dfc467dd2783fc2cc595
Yun-Sheng Chang <yschang@mit.edu>
2025-03-26 20:22:34 -0400

    Add populate.sh for retwis

---
+++ main/tulip-retwis/populate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Populate the YCSB database.
+
+CONF_DIR=../gen-conf/
+CONF=${CONF:-${CONF_DIR}/conf-localhost.json}
+
+RKEYS=${RKEYS:-1000000}
+
+go build && ./tulip-retwis -conf  $CONF \
+						   -rkeys $RKEYS \
+						   -populate
