a6a47603dfec0f1ff18ec29d391e8f872a962f19
Yun-Sheng Chang <yschang@mit.edu>
2025-03-31 14:08:57 -0400

    Use wide-area config for continuous profiling

--- main/tulip-cont/run.sh
+++ main/tulip-cont/run.sh
@@ -3,9 +3,9 @@
 # Execute the continuous profiling benchmark.
 
 CONF_DIR=../gen-conf/
-CONF=${CONF:-${CONF_DIR}/conf-localhost.json}
+CONF=${CONF:-${CONF_DIR}/conf-wide.json}
 
-DURATION=${DURATION:-5}
+DURATION=${DURATION:-60}
 
 STEP=${STEP:-1000}
 
