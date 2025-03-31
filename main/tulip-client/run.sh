d216160eb8751bfbd338d06f6fb65db51d170ed3
Yun-Sheng Chang <yschang@mit.edu>
2025-03-30 21:24:30 -0400

    Use datacenter config for tulip-client

--- main/tulip-client/run.sh
+++ main/tulip-client/run.sh
@@ -3,6 +3,6 @@
 # Terminal program to issue user/mod requests.
 
 CONF_DIR=../gen-conf/
-CONF=${CONF:-${CONF_DIR}/conf-localhost.json}
+CONF=${CONF:-${CONF_DIR}/conf-datacenter.json}
 
 go build && ./tulip-client $CONF 0
