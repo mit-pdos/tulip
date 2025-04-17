414330162cfa538736ddc53b34fbe3b1107731d2
Yun-Sheng Chang <yschang@mit.edu>
2025-04-17 03:24:07 -0400

    Update cloc.sh

--- scripts/cloc.sh
+++ scripts/cloc.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-cloc --include-lang=Go --exclude-dir="main,txnpaxos" .
+CLOC="cloc --include-lang=Go --quiet"
+
+$CLOC --exclude-dir="main,txnpaxos,netsim" .
+
+echo "Transaction:"
+$CLOC ./txn ./gcoord
+
+echo "Backup coordinator:"
+$CLOC ./backup
+
+echo "Replica:"
+$CLOC ./replica
+
+echo "Transaction log:"
+$CLOC ./txnlog
+
+echo "Paxos:"
+$CLOC ./paxos
+
+echo "Tuple:"
+$CLOC ./tuple ./index
+
+echo "Message:"
+$CLOC ./message
+
+echo "Misc:"
+$CLOC ./util ./quorum ./params ./tulip ./trusted_proph ./trusted_time
